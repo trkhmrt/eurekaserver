22c7591ab295703d0ea44396d9626c3541cb6544
Tarık Hamarat <80787831+trkhmrt@users.noreply.github.com>
2026-02-28 22:27:20 +0300

    Add files via upload

---
+++ src/test/java/com/ael/eurekaserver/eurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ael.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class eurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
